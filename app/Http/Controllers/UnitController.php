f63cdbd1408536efaca73a034da8755ff8d0552e
benseghirhoussemeddine <houssem.macbook@gmail.com>
2020-01-15 19:56:24 +0100

    add unit validation

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Session;
 class UnitController extends Controller
 {
     /*
-     *return:view
+     * method:index
      * */
     public function index(){
         //$units = Unit::all();
@@ -19,11 +19,48 @@ class UnitController extends Controller
             'units' => $units
         ]);
     }
+    /*
+     * method:check Unit name if exist
+     * */
+    private function unitNameExists($unitName){
+        $unit = Unit::where(
+            'unit_name','=',$unitName
+        )->first();
+        if(!is_null($unit)){
+            Session::flash('message','unit Name('.$unitName.') already exist');
+            return false;
+        }
+        return true;
+    }
+    /*
+     * method:check Unit code if exist
+     * */
+    private function unitCodeExists($unitCode){
+        $unit = Unit::where(
+            'unit_code','=',$unitCode
+        )->first();
+        if(!is_null($unit)){
+            Session::flash('message','unit Code('.$unitCode.') already exist');
+            return false;
+        }
+        return true;
+    }
+    /*
+     * method:store
+     * */
     public function store(Request $request){
         $request->validate([
             'unit_name' => 'required',
             'unit_code' => 'required'
         ]);
+        $unitName = $request->input('unit_name');
+        $unitCode = $request->input('unit_code');
+        if(!$this->unitNameExists($unitName)){
+            return redirect()->back();
+        }
+        if(!$this->unitCodeExists($unitCode)){
+            return redirect()->back();
+        }
         $unit = new Unit();
         $unit->unit_name = $request->input('unit_name');
         $unit->unit_code = $request->input('unit_code');
@@ -32,6 +69,9 @@ class UnitController extends Controller
         return redirect()->back();
         //1:04:00
     }
+    /*
+     * method:delete
+     * */
     public function delete(Request $request){
         //TODO:check if the id already exist
 
@@ -44,6 +84,10 @@ class UnitController extends Controller
         Session::flash('message' , 'unit has been deleted');
         return redirect()->back();
     }
+
+    /*
+    * method:update
+    * */
     public function update(Request $request){
         //TODO: update the given unit
         $request->validate([
@@ -51,6 +95,14 @@ class UnitController extends Controller
             'unit_name' => 'required',
             'unit_code' => 'required'
         ]);
+        $unitName = $request->input('unit_name');
+        $unitCode = $request->input('unit_code');
+        if(!$this->unitNameExists($unitName)){
+            return redirect()->back();
+        }
+        if(!$this->unitCodeExists($unitCode)){
+            return redirect()->back();
+        }
         $unitID = intval($request->input('unit_id'));
         $unit = Unit::find($unitID);
         $unit->unit_name = $request->input('unit_name');
@@ -58,11 +110,11 @@ class UnitController extends Controller
         $unit->save();
         Session::flash('message' , 'unit ' .$unit->unit_name.' has been updated');
         return redirect()->back();
-
-
-
-
     }
+
+    /*
+    * method:search
+    * */
     public function search(Request $request){
         //TODO:make search for given unit
     }
